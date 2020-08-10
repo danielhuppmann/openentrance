340cf4c5e3bff99d669f8e01abbd78905c68b417
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2020-08-10 15:53:39 +0200

    Update import command

--- nomenclature/tests/test_transform_functionality.py
+++ nomenclature/tests/test_transform_functionality.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pyam import IamDataFrame
-from nomenclature import time_to_year_subannual
+from transform_IamDataFrame import time_to_year_subannual
 
 
 TEST_DF = pd.DataFrame([
