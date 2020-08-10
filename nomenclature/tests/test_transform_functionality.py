7ed714340b755f34e0c42ff15cd805cfa6113f52
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2020-08-10 15:47:26 +0200

    Update and fix typos in test function

--- nomenclature/tests/test_transform_functionality.py
+++ nomenclature/tests/test_transform_functionality.py
@@ -5,7 +5,7 @@ from nomenclature import time_to_year_subannual
 
 TEST_DF = pd.DataFrame([
     ['model_a', 'scen_a', 'Europe', 'Primary Energy', 'EJ/yr', 
-     '2015-01-01T00:00+01:00', 11231.088],
+     '2015-01-01T00:00:00+01:00', 11231.088],
 ],
     columns=['model', 'scenario', 'region', 
              'variable', 'unit', 'time', 'value'])
@@ -14,8 +14,8 @@ df = IamDataFrame(TEST_DF)
 
 def test_transformation_IamDataFrame():
     # test transformation (cast) from IamDataFrame
-    assert (time_to_year_subannual['year'] == 2015 
-            and time_to_year_subannual['subannual'] == '01-01T00:00+01:00')
+    to_test = time_to_year_subannual(df).as_pandas()
+    assert (to_test['year'] [0]==2015 and to_test['subannual'][0]=='01-01 00:00:00+01:00')
 
 
 
